63b181b788ba7bb4a73d6f0ff2c28b845aa33341
lyan62 <wenyanl62@gmail.com>
2024-06-07 17:09:54 +0800

    update

---
+++ model-eval/scripts/eval_qwen_textqa.py
@@ -0,0 +1,118 @@
+import requests
+import torch
+from PIL import Image
+from io import BytesIO
+import yaml
+import os
+import json
+from tqdm import tqdm 
+
+from modelscope import snapshot_download
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+import textqa_utils
+import utils
+import argparse
+from tqdm import tqdm 
+
+class Evaluator(object):
+    def __init__(self, args):
+        self.model_name = args.model_name
+    
+    def _load_model(self):
+        model = AutoModelForCausalLM.from_pretrained(
+            "Qwen/Qwen2-7B-Instruct",
+            torch_dtype="auto",
+            device_map="auto"
+        )
+        processor = AutoTokenizer.from_pretrained("Qwen/Qwen2-7B-Instruct")
+        return model, processor
+    
+    def eval_question(self, textqa, idx, model, processor, args):
+        question = textqa[idx]
+        query_list = textqa_utils.get_prompt_qwen(question, template=args.template, lang=args.lang)
+        
+        text = processor.apply_chat_template(
+            query_list,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        model_inputs = processor([text], return_tensors="pt").to("cuda")
+
+        generated_ids = model.generate(
+            model_inputs.input_ids,
+            max_new_tokens=512
+        )
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+
+        response = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        
+        return {
+            "response": response,
+            "qid": textqa[idx]["qid"]
+        }
+            
+
+
+
+def main(args):
+    # load model and processor
+    evaluator = Evaluator(args)
+    
+    model, processor = evaluator._load_model()
+    
+
+    # read_data
+    data_dir = args.data_dir
+    eval_file = args.eval_file
+    template = args.template
+    out_dir = args.out_dir
+    
+    textqa = textqa_utils.read_textqa(data_dir)
+    
+    if "Mantis" in args.model_name:
+        out_file_name = "textqa_" + 'mantis' + "_prompt" + str(template) + ".jsonl"
+    elif "qwen" in args.model_name:
+        out_file_name = "textqa_" + 'qwen' + "_prompt" + str(template) + ".jsonl"
+    else:
+        out_file_name = "textqa_" + 'idefics' + "_prompt" + str(template) + ".jsonl"
+    os.makedirs(out_dir, exist_ok=True)
+    
+    ## eval
+    print("Evaluating model on {} questions".format(len(textqa)))
+    with open(os.path.join(out_dir, out_file_name), "w") as f:
+        for idx in tqdm(range(len(textqa))):
+            res = evaluator.eval_question(textqa, idx, model, processor, data_dir, args)
+            f.write(json.dumps(res, ensure_ascii=False)+"\n")
+            
+    print("Saved model response to %s"%out_file_name)
+    # print("Calculate accuracy...")
+    # accuracy = utils.get_accuracy(os.path.join(out_dir, out_file_name), 
+    #                               sivqa, parse_fn=utils.parse_idefics_sivqa)
+    # print(accuracy)
+    
+    
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--cache_dir", default="/scratch3/wenyan/cache")
+    argparser.add_argument("--data_dir", default="/scratch3/wenyan/data/foodie")
+    argparser.add_argument("--eval_file", default="sivqa_filtered.json")
+    argparser.add_argument("--out_dir", default="/scratch3/wenyan/data/foodie/results")
+    argparser.add_argument("--model_name", default="qwen")
+    argparser.add_argument("--show_food_name", action="store_true", default=False)
+    argparser.add_argument("--template", type=int, default=0)
+    argparser.add_argument("--lang", default="zh")
+    
+    args = argparser.parse_args()
+
+    os.environ["HF_HOME"] = args.cache_dir
+    # load_model
+    torch.cuda.empty_cache()
+    
+    main(args)
+    
+    
+    
---
+++ model-eval/scripts/textqa_utils.py
@@ -0,0 +1,99 @@
+# Human: {question} Here are the options: {options} Assistant: If had to select one of the options, my answer would be (
+import json
+import os
+
+
+def read_textqa(data_dir):
+    question_file = os.path.join(data_dir, "textqa_filtered.json")
+    with open(question_file, 'r', encoding='utf-8') as f:
+        sivqa = json.load(f)
+    return sivqa
+
+def format_choices(choices, template=0):
+    idx2choice = {0:"A", 1:"B", 2:"C", 3:"D"}
+    choices_str = ''
+    for idx, choice in enumerate(choices):
+        choices_str += "（{}) {}\n".format(idx2choice[idx], choice.strip())
+    return choices_str
+
+def format_question(question):
+    q = question["question"].strip()
+    choices = question["choices"]
+    
+    choices_str = format_choices(choices)
+    
+    return q, choices_str
+
+def format_text_prompt(q, choices_str, template=0, lang="zh"):
+    if lang == "zh":
+        if template == 0:
+            return "{} 选项有: {}, 请根据从所提供的选项中选择一个正确答案，为（".format(q, choices_str)
+        if template == 1:
+            return "你是一个智能助手，请你回答以下选择题：{} 选项有: {}, 请从中选择一个正确答案，为（".format(q, choices_str)
+        if template == 2:
+            return "你是一个智能助手，现在回答以下选择题：{} 选项有: {}\n".format(q, choices_str) + \
+                "智能助手：我从所提供的选项中选择一个正确答案，为（"
+            # return "用户：你是一个智能助手，现在请看图回答以下选择题：{} 选项有: {}, 智能助手：我从所提供的选项中选择一个正确答案，为（".format(q, choices_str)
+        if template == 3:
+            return "{} 这是选项: {} 请从所提供的选项中选择一个正确答案。请保证你的答案清晰简洁并输出字母选项。\n".format(q, choices_str)+ \
+                "智能助手：我选择（"
+            # return "用户：{} 这是选项: {} 请根据上图从所提供的选项中选择一个正确答案。智能助手：我选择（".format(q, choices_str)
+    else:
+        if template == 0:
+            return "{} Here are the options: {} If I had to select one of the options, my answer would be (".format(q, choices_str)
+        if template == 1:
+            return "You are an AI assistant. Please answer the following multiple choice question: {} Here are the options: {} Please select one of the options as your answer (".format(q, choices_str)
+        if template == 2:
+            return ["{} Here are the options: {}".format(q, choices_str), "If I had to select one of the options, my answer would be ("] 
+            # return "Human: {} Here are the options: {} Assistant: If had to select one of the options, my answer would be (".format(q, choices_str)
+        if template == 3:
+            return ["{} These are the options: {} Please select one of the options as your answer.".format(q, choices_str), "I would select ("]
+            # return "Human: {} These are the options: {} Please select one of the options as your answer. Assistant: I would select (".format(q, choices_str)
+        
+
+def get_prompt_qwen(question, template=0, lang="zh"):
+    # for qwen model
+    q, choices_str = format_question(question)
+
+    text_prompt = format_text_prompt(q, choices_str, template, lang=lang)
+    
+    if isinstance(text_prompt, list):
+        if lang == "zh":
+            messages = [
+                {"role": "system", "content": "你是一个智能助手。"},
+                {"role": "user", "content": text_prompt.replace("你是一个智能助手，", "")}] # avoid repeating from the system prompt
+        else:
+            raise NotImplementedError
+
+    return messages
+
+def get_prompt_idefics(question, data_dir, show_food_name=False, template=0, lang="zh"):
+    # for both idefics2 and mantis
+    q, img, choices_str = format_question(question, show_food_name)
+    text_prompt = format_text_prompt(q, choices_str, template, lang=lang)
+    if isinstance(text_prompt, list):
+        query_list = [
+                    {
+                        "role": "user",
+                        "content": [
+                            {"type": "image", "image": os.path.join(data_dir, img)},
+                            {"type": "text", "text": text_prompt[0]}
+                        ]
+                    },
+                    {
+                        "role": "assistant",
+                        "content": [
+                            {"type": "text", "text": text_prompt[1]}
+                        ]
+                    }
+                ]
+    else:
+        query_list = [
+                        {
+                            "role": "user",
+                            "content": [
+                                {"type": "image"},
+                                {"type": "text", "text": text_prompt},
+                            ]}
+                        ]
+    return query_list
